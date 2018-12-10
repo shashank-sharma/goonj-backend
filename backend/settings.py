62743c8cae0b8408f03633b2ceff06eeece64cbd
Shashank Sharma <shashank.sharma98@gmail.com>
2018-12-10 18:07:20 +0530

    Static files update

--- backend/settings.py
+++ backend/settings.py
@@ -135,6 +135,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_PERMISSION_CLASSES': (
