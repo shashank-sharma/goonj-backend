ac04a0eec351441f62c9eaaa6dca4bd71fba00f3
Shashank Sharma <shashank.sharma98@gmail.com>
2019-02-03 12:56:45 +0530

    CORS Header bug

--- backend/settings.py
+++ backend/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'accounts',
     'government_data',
     'goonj_sessions',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'accounts.middleware.UpdateLastActivityMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'backend.urls'
 
